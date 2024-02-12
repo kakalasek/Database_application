e6d1c5e76e29bd90486a2169e0c2b8258e816047
kakalasek <honzicekkj@seznam.cz>
2024-02-12 23:07:52 +0100

    somewhat working GridBagLayout

--- src/main/java/Windows/Search.java
+++ src/main/java/Windows/Search.java
@@ -1,8 +1,12 @@
 package Windows;
 
 import ComponentHandlers.*;
+import Constants.Constants;
 
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
 import java.awt.*;
 
 /**
@@ -19,7 +23,52 @@ public class Search extends JFrame {
 
         this.setContentPane(PanelHandler.create(new GridBagLayout()));
 
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+
+        JPanel upper = new JPanel();
+        //upper.setPreferredSize(new Dimension(0, 50));
+        upper.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 3));
+
+        JLabel genericLabel = new JLabel("Rodové jméno:");
+        genericLabel.setFont(Constants.DEFAULT_FONT);
+        upper.add(genericLabel);
+
+        JTextField genericInput = new JTextField();
+        genericInput.setPreferredSize(new Dimension(150, 25));
+        upper.add(genericInput);
+
+        JLabel speciesLabel = new JLabel("Druhové jméno:");
+        speciesLabel.setFont(Constants.DEFAULT_FONT);
+        upper.add(speciesLabel);
+
+        JTextField speciesInput = new JTextField();
+        speciesInput.setPreferredSize(new Dimension(150, 25));
+        upper.add(speciesInput);
+
+        JButton search = new JButton("Hledat");
+        upper.add(search);
+
+        upper.setBorder(new CompoundBorder(BorderFactory.createMatteBorder(0,0,2,0, Color.black), BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+
+        this.getContentPane().add(upper, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridwidth = 5;
+        gbc.gridy = 1;
+
+        JPanel main= new JPanel();
+        main.setPreferredSize(new Dimension(980,620));
+
+       this.getContentPane().add(main, gbc);
+
         /* Making the frame visible */
+        this.pack();
         this.setVisible(true);
     }
 
